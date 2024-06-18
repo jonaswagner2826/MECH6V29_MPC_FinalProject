de6da059a03079ac498c5abde0e09a1e45f640b8
Jonas Wagner <jonas.wagner.2826@gmail.com>
2024-06-18 16:14:04 -0500

    final commit

--- code/MECH4310_FinalProject.m
+++ code/MECH4310_FinalProject.m
@@ -6,21 +6,12 @@ addpath(genpath(pwd));
 fig_subfolder = strcat(filepath,filesep,'figs');
 data_subfolder = strcat(filepath,filesep,'data');
 
-
-% % sim settings
-% dt = 0.1; tf = 5;
-% tspan = 0:dt:tf;
-
-
 %% CT-Model (currently using kinnematic model)
 % bikeModel_CT(x,u,params)
 nx = 3;
 nu = 2;
 sys_fun = @bikeModel_CT;
 x0 = zeros(nx,1);
-
-% update_eq = @(x,u,dt) sys_fun(x,u).*dt;%ode45(@(t,x) sys_fun(x,u),dt,x0);
-
 
 % constraints
 % (max/min of U_set)
@@ -59,24 +50,24 @@ U_dot_set = Polyhedron('lb',bounds.u_dot_lb, 'ub',bounds.u_dot_ub); U_dot_set.mi
 % The pixels are 0.4m and the whole cost map is 60m by 60m.
 
 
-% % load from cost-map png
-% costMap_filename = 'costmap1.png';
-% costMap_filename = strcat(data_subfolder,filesep,costMap_filename);
-% costMap = 1 - rescale(im2gray(imread(costMap_filename)));
-% 
-% 
-% mapX = -30 + (0.2:0.4:60); % 60m/2 = 30m
-% mapY = -20 + (0.2:0.4:60); % 50px = 20m
-% [gridX,gridY] = ndgrid(mapX,mapY);
-% costMap_fun = griddedInterpolant(gridX,gridY,rot90(costMap,3));
-% surf(gridX,gridY,costMap_fun(gridX,gridY))
-% xlabel('x');
-% ylabel('y');
-% 
-% % loc2px = @(x,y) round([(x+30)/0.4; (y+20)/0.4]);
-% % px2cost = @(px) costMap(end-px(2),px(1));
-% costMap_fun = @(x,y) interp2(gridX,gridY,rot90(costMap,3),x,y,'nearest');
+% load from cost-map png
+costMap_filename = 'costmap1.png';
+costMap = 1 - rescale(im2gray(imread(strcat(data_subfolder,filesep,costMap_filename))));
 
+
+mapX = -30 + (0.2:0.4:60); % 60m/2 = 30m
+mapY = -20 + (0.2:0.4:60); % 50px = 20m
+[gridX,gridY] = ndgrid(mapX,mapY);
+costMap_fun = griddedInterpolant(gridX,gridY,rot90(costMap,3));
+surf(gridX,gridY,costMap_fun(gridX,gridY))
+xlabel('x');
+ylabel('y');
+title('Cost Map Function Visualization')
+saveas(gcf,strcat(fig_subfolder,filesep,'surf_',costMap_filename));
+
+% loc2px = @(x,y) round([(x+30)/0.4; (y+20)/0.4]);
+% px2cost = @(px) costMap(end-px(2),px(1));
+costMap_fun = @(x,y) interp2(gridX,gridY,rot90(costMap,3),x,y,'linear');
 
 %% MPC Controller Setup
 dt_MPC = 1;
@@ -91,9 +82,10 @@ constraints = []; objective = 0;
 constraints = [constraints, U_dot_set.A*((u_{1}-u_0)/dt_MPC) <= U_dot_set.b];
 
 for k = 1:N
-    % objective = objective - x_{k}(1) + (x_{k}(2)-0).^2 + (x_{k}(3) - pi)^2;
-    % objective = objective + interp2(gridX,gridY,rot90(costMap,3),x_{k}(1),x_{k}(2),'nearest');%costMap_fun(x_{k}(1),x_{k}(2));
-    objective = objective + x_{k}(1) + (x_{k}(3)-pi).^2;
+    % objective = objective - x_{k}(2) + (x_{k}(1)-0).^2 + (x_{k}(3) - pi)^2;
+    % objective = objective + costMap_fun(x_{k}(1),x_{k}(2));
+    % objective = objective + x_{k}(1) + (x_{k}(3)-pi).^2;
+    % objective = objective + 1e-2*(x_{k}(2) - 0).^2;
 
     constraints = [constraints, x_{k+1} == update_eq(x_{k},u_{k},dt_MPC,sys_fun)];
     constraints = [constraints, U_set.A*u_{k} <= U_set.b];
@@ -103,17 +95,21 @@ for k = 1:N
     end
 end
 
-objective = objective + x_{N+1}(1) + (x_{N+1}(3)-pi)^2; %<--- minimize these things...
-% objective = objective + costMap_fun(x_{N+1}(1),x_{N+1}(2));
-% objective = objective + interp2(gridX,gridY,rot90(costMap,3),x_{N+1}(1),x_{N+1}(2),'nearest');%costMap_fun(x_{k}(1),x_{k}(2));
+% objective = objective - x_{N+1}(2);% + (x_{N+1}(3)-pi)^2; %<--- minimize these things...
+% objective = objective + (x_{N+1}(1) - 0).^2;
+objective = objective + costMap_fun(x_{N+1}(1),x_{N+1}(2));
+% objective = objective + interp2(gridX,gridY,rot90(costMap,3),x_{N+1}(1),x_{N+1}(2),'linear');%costMap_fun(x_{k}(1),x_{k}(2));
 
 % opts = sdpsettings('solver','ipopt');
-opts = sdpsettings('solver','fmincon','debug',0,'verbose',0);
+opts = sdpsettings('solver','ipopt','debug',0,'verbose',1);
 % opts = sdpsettings;
 controller = optimizer(constraints, objective, opts, {x_{1},u_0}, [u_{1}]);
 
 
 %% MPC controller testing
+runTesting = false;
+
+if runTesting
 
 % sim settings
 dt = 0.1; tf = 10;
@@ -123,7 +119,8 @@ u0 = zeros(nu,1);
 
 timerVal = tic;
 % Initial Conditions
-X_{1} = x0; U_{1} = controller{x0,u0};
+X_{1} = x0; 
+U_{1} = controller{x0,u0};
 % Time Evolution
 for k = 1:length(tspan)-1
     % Simulate Update Step
@@ -137,16 +134,11 @@ end
 
 x = [X_{:}]; u = [U_{:}];
 
-testName = 'Min X with +180 (fmincon)';
+testName = 'Max Y - Final (fmincon)';
 
 
 plot_vehicleModel(tspan,x,u,bounds, ...
     filename=strcat(fig_subfolder,filesep,strrep(testName,' ','_')), ...
     title=strcat(testName));
 
-
-
-
-
-
-
+end
